7fab7f93ebce9414866850be5b1f7382175d47cb
Salil Khanna <salilkhannawork@gmail.com>
2021-04-30 20:12:03 -0400

    added animation to item buyers

--- src/components/ItemBuyer.js
+++ src/components/ItemBuyer.js
@@ -1,7 +1,7 @@
 import Button from './Button'
+import { useSpring, animated as a} from 'react-spring'
 
-
-const ItemBuyer = ({itemName, rateOfProd, currentCount, currentCost, onClick, saladCount}) => {
+const ItemBuyer = ({itemName, rateOfProd, currentCount, currentCost, onClick, saladCount, delayAmount}) => {
 
 
     function roundTo(num) {
@@ -25,13 +25,16 @@ const ItemBuyer = ({itemName, rateOfProd, currentCount, currentCost, onClick, sa
       }
 
     return (
-    <div className = "header2">
+      <a.div style = {useSpring({from: { opacity: 0 }, to: { opacity: 1 }, config: { duration: 1000 }, delay: delayAmount, }) }>
+<div className = "header2">
         <p > <b>{itemName}</b>: Produces {rateOfProd} Salads per Second (Current Count = {currentCount}) </p>
         <Button text = {"Click to Buy For " + adjDisp(currentCost) + " salads"} onClick={onClick} 
           color = {saladCount >= currentCost ? "green" : "red"}
         />
       </div>
-    )
+      </a.div>
+          )
+    
 }
 
 export default ItemBuyer
